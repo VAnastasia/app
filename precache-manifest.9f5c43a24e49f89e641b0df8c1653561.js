04405d7a21f59d901368b1a06e4a32611bd21b14
Anastasiya <v.anastasiya26@mail.ru>
2020-01-10 11:18:06 +0700

    Updates

---
+++ precache-manifest.9f5c43a24e49f89e641b0df8c1653561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87a78cdf851051688fa32e6dbc6b701",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "11c50cd272f4f85abae2",
+    "url": "/app/static/css/main.84fa0e5d.chunk.css"
+  },
+  {
+    "revision": "26896646bd83d6b86905",
+    "url": "/app/static/js/2.3ab078f4.chunk.js"
+  },
+  {
+    "revision": "11c50cd272f4f85abae2",
+    "url": "/app/static/js/main.4066bb10.chunk.js"
+  },
+  {
+    "revision": "d05c0a14a6451fd8c963",
+    "url": "/app/static/js/runtime-main.a8cd627d.js"
+  },
+  {
+    "revision": "e35acbe602ad4621ee0ae10b135eb05e",
+    "url": "/app/static/media/down-icon.e35acbe6.svg"
+  },
+  {
+    "revision": "9df51d4e640dadfd9884b1a61c7b5642",
+    "url": "/app/static/media/search-icon.9df51d4e.svg"
+  }
+]);
